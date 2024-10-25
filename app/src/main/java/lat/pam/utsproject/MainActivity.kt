5e52261b67dca9575317ac302da58a4f4b684fad
Reyhan Salman <118980086+Ryhnslmn13@users.noreply.github.com>
2024-10-25 14:31:52 +0700

    Page 1 dan 2

---
+++ app/src/main/java/lat/pam/utsproject/MainActivity.kt
@@ -0,0 +1,44 @@
+package lat.pam.utsproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val username = findViewById<EditText>(R.id.etUsername)
+        val password = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnLogin.setOnClickListener {
+            val username = username.text.toString()
+            val password = password.text.toString()
+
+            if (username == "cust" && password == "cust123") {
+                Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, ListFoodActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Login failed. Check your credentials.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
